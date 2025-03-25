318d37203e22a8d01c99733924b708a7a531e862
kanatayo <2558156825@qq.com>
2025-03-25 19:37:44 +0800

    实现分类，list页面

--- video/urls.py
+++ video/urls.py
@@ -1,8 +1,10 @@
 
 from django.urls import path
 
-from .views import index
+from .views import index, video_list, video_details
 
 urlpatterns = [
-    path('index/', index)
+    path('index/', index),
+    path('list/<str:categoryName>', video_list),
+        path('details/<int:video_id>', video_details)
 ]
--- video/views.py
+++ video/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from video.models import Video
+from video.models import Video, Category
 
 
 # Create your views here.
@@ -9,6 +9,16 @@ from video.models import Video
 def index(request):
     videos = Video.objects.filter(category__categoryName='电视剧')
     print(videos)
-    return render(request, 'category_video.html', {'videos': videos})
+    return render(request, 'index.html', {'videos': videos})
 
 
+def video_list(request, categoryName):
+    category = Category.objects.all()
+    videos = Video.objects.filter(category__categoryName=categoryName)
+    print(videos)
+    return render(request, 'list.html', {'category': category, 'videos': videos})
+
+
+def video_details(request, video_id):
+    video = Video.objects.get(id=video_id)
+    return render(request, 'details.html', {'video': video})
